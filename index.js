b8052b8183176330e8737c4c76a13df208ab6ed9
Peejay Gathaiya <gathaiya.peejay@gmail.com>
2023-10-15 12:20:25 +0300

    Done w Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('input')
+
+    function clickHere(){
+        alert('You clicked on this button!')
+    }
+
+    input.addEventListener('click', clickHere)
+
+    // input.addEventListener('click', function(){
+    //     alert('I was clicked')})
 }
